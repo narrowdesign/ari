e1a18a57a5a16a59d1331f74d86893bc9e02927d
narrowdesign <nick@narrowdesign.com>
2020-09-02 08:20:00 -0400

    logo color

--- ui/assets/scripts/script.js
+++ ui/assets/scripts/script.js
@@ -520,7 +520,7 @@ $(function() { // INITIALIZE AFTER JQUERY IS LOADED
 
   function resizeHandler () { // Set the size of images and preload them
     _winW = window.innerWidth;
-    _winH = window.innerHeight;
+    _winH = window.clientHeight;
     revealBuffer = 230;
     if(_winW > 960){
       smallScreen = false;
